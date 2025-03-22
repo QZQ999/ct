f97724f9b9cf189645948ff8a745f734decb03e3
QZQ999 <tienyu.zuo@seu.edu.cn>
2025-03-23 00:16:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+if __name__ == '__main__':
+    print('Hello World')
+    
+    
---
+++ model/utils.py
@@ -0,0 +1,9 @@
+'''
+Author: tienyu tienyu.zuo@nuist.edu.cn
+Date: 2025-03-23 00:05:37
+LastEditors: tienyu tienyu.zuo@nuist.edu.cn
+LastEditTime: 2025-03-23 00:11:16
+FilePath: \ct\model\utils.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+1
